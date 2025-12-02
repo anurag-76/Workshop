204ac5cd78a1fd3d40885f7d552026e8d1380b78
anurag-76 <anurag0948@gmail.com>
2025-12-02 10:02:22 +0545

    learnt java script

---
+++ script.js
@@ -0,0 +1,88 @@
+// console.log('Hello World');
+
+// console.log("Name: Anurag")
+// console.log("Interest:Coding")
+
+// // 2. Difference between var , let , const
+// let x = 32;
+// var y = "Hello guys";
+// const z =  true;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+
+// // 3. program
+// let a = 1;
+// if(a>5){
+//     a=20;
+// }
+// else{
+//     a=30;
+// }
+// console.log(a);
+
+// 4. Write a program that determines whether a given number is positive or negative.
+
+// let num =  prompt("Enter a number : ");
+// if(num>0){
+//     console.log('It is a positive number');
+// }
+// else{
+//     console.log('It is a negative number');
+// }
+
+// let i;
+// for (i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz");
+//     }
+//     else if (i % 3 == 0) {
+//         console.log("FIZZ");
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("BUZZ")
+//     }
+
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// const num1 = Number(prompt("Enter a number : "));
+// const num2 = Number(prompt("Enter another number : "));
+// const operator = prompt("Enter operator (+, -, * or /):");
+// let output;
+// switch (operator) {
+//     case "+":
+//     output = num1 + num2;
+//         break;
+
+//     case "-":
+//     output = num1 - num2;
+//         break;
+
+//     case "*":
+//     output = num1 * num2;
+//         break;
+
+//     case "/":
+//     output = num1 / num2;
+//         break;
+
+//     default :
+//     output= "Operator is invalid";
+// }
+// alert("The result is : " + output);
+
+//Write a for loop that prints numbers from 11 to 19.
+// let l;
+// for(l=11; l<=19 ;l++){
+//     console.log(l)
+// }
+
+let RandomNum = 10;
+while (RandomNum >= 1) {
+    console.log(RandomNum - 1);
+}
